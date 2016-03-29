470d0bae7cdfd66f2fde82aa6827208910130fb0
Reynold Erwandi <reynolderwandi@gmail.com>
2016-03-29 15:37:33 +0700

    Add fibonacci script

---
+++ Fibbonaci.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibbonaci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Fibbonaci {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Jumlah : ");
+        int n = input.nextInt();
+        
+        for (int i = 1; i <= n; i++)
+            System.out.print(fibbonaci(i) + " ");
+    }
+    
+    public static int fibbonaci(int n) {
+        if (n <= 1) return n;
+        else return fibbonaci(n-1) + fibbonaci(n-2);
+    }
+    
+}
